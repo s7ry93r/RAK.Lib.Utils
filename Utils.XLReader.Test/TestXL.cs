7a630e25635b725172f6c2627cd912a5dc3c1a17
s7ry93r <rkrueger@gmail.com>
2019-08-25 07:55:12 -0400

    Better Test, Got Sql Parser working, Started Excel Recon

---
+++ Utils.XLReader.Test/TestXL.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+using System.IO;
+
+namespace RAK.Lib.Utils.XLReader.Test
+{
+    public class TestXL
+    {
+        private string getCWD()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        private string getParentDir(string pwd)
+        {
+            return (new DirectoryInfo(pwd).Parent).FullName;
+        }
+
+        private string getSqlPath()
+        {
+            var newDir = getParentDir(getParentDir(getParentDir(getCWD())));
+            var sqlPath = Path.Combine(newDir, @"Create.dbo.People.sql");
+            return sqlPath;
+        }
+
+
+        [Fact]
+        public void TestGenerateSQL()
+        {
+            var xlr = new GenerateMapXml(null, getSqlPath());
+            Assert.Equal("Id", xlr.ParseSql.cols[0].Name);
+
+        }
+    }
+}
